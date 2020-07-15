2422ca15491c0fb2607be1331bb9c9bb5324404c
Niraj Kakodkar <niraj.kakodkar@gmail.com>
2020-07-15 10:22:11 +0300

    FlatJson script added

--- core/BOD2FlatJson.py
+++ core/BOD2FlatJson.py
@@ -1,4 +1,5 @@
 import xmltodict, json
+from flatten_json import flatten_json
 
 ## Remove the below line while using in the ION Script
 from config import config
@@ -7,9 +8,9 @@ from config import config
 
 # Change the config.input_var to input_var while using in ION Script
 output_var = xmltodict.parse(config.input_var)
-output_var = json.dumps(output_var)
 
-output_var = json.dumps(json.loads(output_var), indent=4)
+# output_var = json.dumps(json.loads(output_var), indent=4)
+output_var = flatten_json(output_var)
 
 ## Remove the below line while using in the ION Script
 print(output_var)
