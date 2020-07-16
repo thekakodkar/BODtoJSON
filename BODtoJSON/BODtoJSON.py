4774034c98b89dd88c4942e8fc08145aa017259a
Niraj Kakodkar <niraj.kakodkar@gmail.com>
2020-07-16 14:09:14 +0300

    Removed Dependencies

--- BODtoJSON/BODtoJSON.py
+++ BODtoJSON/BODtoJSON.py
@@ -22,10 +22,15 @@
 import xmltodict, json
 import xml.etree.ElementTree as ET
 import re
-from flatten_json import flatten_json
+import six
 
 
 def convert(input_var):
+    """
+    converts the input BOX XML to Flat JASON
+    :param input_var:
+    :return: Flat JSON
+    """
     # Remove Namespaces from XML
     input = re.sub(' xmlns="[^"]+"', '', input_var, count=1)
 
@@ -40,6 +45,62 @@ def convert(input_var):
         root = ET.tostring(parentnode).decode('utf-8')
 
     output_var = xmltodict.parse(root)
-    output_var = flatten_json(output_var)
+    output_var = flatify(output_var)
     output_var = json.dumps(output_var)
     return (output_var)
+
+
+def _genkey(prev_key, separator, new_key):
+    """
+    Returns the new_key in case no prev key exists, concatenates
+    prev key, separator, and new_key otherwise
+    :param prev_key:
+    :param separator:
+    :param new_key:
+    :return: a string if prev_key exists and simply passes through the
+    new_key otherwise
+    """
+    if prev_key:
+        return u"{}{}{}".format(prev_key, separator, new_key)
+    else:
+        return new_key
+
+
+def flatify(idict, separator="_", root_keys_to_ignore=set()):
+    """
+    Creates flat dictionary from the nested dictionary
+
+    :param idict: Nested dictionary we want to make flat
+    :param separator: string to separate dictionary keys by
+    :param root_keys_to_ignore: set of root keys to ignore from flattening
+    :return: flat dictionary
+    """
+    assert isinstance(idict, dict), "flattify requires a dictionary input"
+    assert isinstance(separator, six.string_types), "separator must be string"
+
+    flat_dict = dict()
+
+    def _flatify(object_, key):
+        """
+        :param object_: object to flatten
+        :param key: carries the concatenated key for the object_
+        :return: None
+        """
+        # Empty object can't be iterated, take as is
+        if not object_:
+            flat_dict[key] = object_
+        # These object types support iteration
+        elif isinstance(object_, dict):
+            for object_key in object_:
+                if not (not key and object_key in root_keys_to_ignore):
+                    _flatify(object_[object_key], _genkey(key,
+                                                          separator,
+                                                          object_key))
+        elif isinstance(object_, (list, set, tuple)):
+            for index, item in enumerate(object_):
+                _flatify(item, _genkey(key, separator, index))
+        else:
+            flat_dict[key] = object_
+
+    _flatify(idict, None)
+    return flat_dict
